922023fd44cbdf6e151a4bc5672904cfb9f292e0
Alex <u202315585@upc.edu.pe>
2025-06-13 23:21:23 -0500

    Ultimos cambios?

--- js/compression.js
+++ js/compression.js
@@ -264,7 +264,7 @@ function updateUI(result, fileName) {
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
             <polyline points="6 9 12 15 18 9"></polyline>
           </svg>
-          Mostrar texto completo
+          Mostrar codificación completa
         `;
         encodedFullText.classList.remove('expanded');
       } else {
